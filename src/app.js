a316ccd39f3e85ec712186f9ecf41762b2d28721
Jonathan Palma <166739509+gitttjonzzz@users.noreply.github.com>
2025-09-26 23:48:29 +0000

    domain name draft

--- src/app.js
+++ src/app.js
@@ -6,6 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".btn").addEventListener('click',generateScript)
 };
+const generateScript = () =>{
+    let pronoun = ['the', 'our','what','that','which'];
+    let adjective = ['great', 'big','happy','small','fast'];
+    let noun = ['jogger', 'racoon','teacher','lake','dog'];
+    let extensions=['.com','.net','.us','.io','.org'];
+
+    let pronounDiv =document.getElementById('pronoun');
+    let adjectiveDiv =document.getElementById('adjective');
+    let nounDiv =document.getElementById('noun');
+    let extensionsDiv =document.getElementById('extensions');
+
+    for(let i=0; i < generateScript.length; i++){
+
+        pronounDiv.innerHTML = pronoun[i];
+        adjectiveDiv.innerHTML = adjective[i];
+        nounDiv.innerHTML = noun[i];
+        extensionsDiv.innerHTML = extensions[i];
+
+
+      console.log(pronoun[i] + adjective[i] + noun[i] + extensions[i])
+    }
+}
