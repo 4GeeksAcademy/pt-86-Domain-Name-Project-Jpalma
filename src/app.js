1a6d97f3039e731878b4590d34f307548c83b3e0
Jonathan Palma <166739509+gitttjonzzz@users.noreply.github.com>
2025-09-29 23:57:58 +0000

    Domain Name generator

--- src/app.js
+++ src/app.js
@@ -6,27 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  document.querySelector(".btn").addEventListener('click',generateScript)
+  combineArrays()
 };
-const generateScript = () =>{
-    let pronoun = ['the', 'our','what','that','which'];
-    let adjective = ['great', 'big','happy','small','fast'];
-    let noun = ['jogger', 'racoon','teacher','lake','dog'];
-    let extensions=['.com','.net','.us','.io','.org'];
+const combineArrays =()=> {
+  let pronoun = ['the', 'our','what','that','which'];
+  let adjective = ['great', 'big','happy','small','fast'];
+  let noun = ['jogger', 'racoon','teacher','lake','dog'];
+  let extensions=['.com','.net','.us','.io','.org'];
 
-    let pronounDiv =document.getElementById('pronoun');
-    let adjectiveDiv =document.getElementById('adjective');
-    let nounDiv =document.getElementById('noun');
-    let extensionsDiv =document.getElementById('extensions');
-
-    for(let i=0; i < generateScript.length; i++){
-
-        pronounDiv.innerHTML = pronoun[i];
-        adjectiveDiv.innerHTML = adjective[i];
-        nounDiv.innerHTML = noun[i];
-        extensionsDiv.innerHTML = extensions[i];
-
-
-      console.log(pronoun[i] + adjective[i] + noun[i] + extensions[i])
+  for(let i =0; i <pronoun.length; i++){
+    for(let a =0; a <adjective.length; a++){
+      for(let b =0; b <noun.length; b++){
+        for(let c =0; c <extensions.length; c++){
+            console.log(pronoun[i] + adjective[a] + noun[b] + extensions[c])
+        }
+      }
+     }
     }
-}
+};
+
